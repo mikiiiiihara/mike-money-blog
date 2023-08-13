006695b823d8d08f040892a06aff625401f70150
井原 樹/Miki Ihara <mi-ihara@tg-inet.co.jp>
2023-08-13 11:41:59 +0900

    profile文更新

--- libs/renderToc.ts
+++ libs/renderToc.ts
@@ -2,7 +2,7 @@ import * as cheerio from "cheerio";
 
 export const renderToc = (body: string) => {
   const $ = cheerio.load(body);
-  const headings = $("h2, h3, h4").toArray();
+  const headings = $("h2, h3").toArray();
   let toc: { text: string; id: string; tagName: string }[] = [];
   headings.forEach((data) => {
     if (data.children && data.children[0]) {
