78b025abb1253d69c49c2972fd174e0df1214747
井原 樹/Miki Ihara <mi-ihara@tg-inet.co.jp>
2023-08-15 07:09:55 +0900

    ogpのdescription修正

--- constants/constants.ts
+++ constants/constants.ts
@@ -2,7 +2,7 @@
 export const PER_PAGE = 4;
 // サイト説明
 export const COMMON_DESCRIPTION =
-  "お得に海外旅行するための情報、おすすめ旅行先(時々K-POP)をお届け！";
+  "20代向け資産形成術を発信中！海外旅行で散財しながら3年で400万円貯めた私が、節約・本業の収入を増やす・投資の３本柱で20代を楽しみながら将来にも備える方法をお届け！";
 
 // 広告欄
 export const AD_CONTENT = `<img border="0" width="1" height="1" src="https://www11.a8.net/0.gif?a8mat=3TAVQ2+DDNP6A+52F8+5ZMCH" alt=""><h3>旅費を安くしたい方へ</h3><a href="https://pc.moppy.jp/entry/invite.php?invite=fXXBe1cd&type=service"><img decoding="async" loading="lazy" src="https://img.moppy.jp/pub/pc/friend/300x250-1.jpg" alt="累計会員数1,000万人突破！内職/副業/お小遣い稼ぎするならモッピー！" width="300" height="250"></a>`;
